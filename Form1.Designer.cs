a7bc97f45abf8381407c71f5298ac9f47963a2b4
Jim Herd <jtherd@gmail.com>
2020-07-17 17:52:22 +0100

    Moved FPGA_uP_IO code to a class library

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.COM_init = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
@@ -42,8 +45,11 @@
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.COM_init.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,7 +65,9 @@
             // 
             // COM_init
             // 
-            this.COM_init.Controls.Add(this.button2);
+            this.COM_init.Controls.Add(this.label3);
+            this.COM_init.Controls.Add(this.label2);
+            this.COM_init.Controls.Add(this.comboBox2);
             this.COM_init.Controls.Add(this.button1);
             this.COM_init.Controls.Add(this.comboBox1);
             this.COM_init.Location = new System.Drawing.Point(4, 25);
@@ -71,11 +79,44 @@
             this.COM_init.UseVisualStyleBackColor = true;
             this.COM_init.Click += new System.EventHandler(this.tabPage1_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "List of COM Ports";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "List of BAUD rates";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "9600",
+            "115200",
+            "128000",
+            "230400",
+            "256000"});
+            this.comboBox2.Location = new System.Drawing.Point(205, 63);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 3;
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(38, 65);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(68, 104);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.Size = new System.Drawing.Size(209, 38);
             this.button1.TabIndex = 1;
             this.button1.Text = "Open COM Port";
             this.button1.UseVisualStyleBackColor = true;
@@ -84,13 +125,16 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(38, 94);
+            this.comboBox1.Location = new System.Drawing.Point(38, 63);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 0;
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.button2);
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -153,13 +197,33 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(273, 79);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(35, 65);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(126, 60);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 61);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Soft Ping";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "label4";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(266, 65);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 61);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Hard Ping";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -176,6 +240,9 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.COM_init.ResumeLayout(false);
+            this.COM_init.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -197,6 +264,11 @@
         private System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -4,105 +4,126 @@ using System.Windows.Forms;
 using System.IO.Ports;
 using System.Threading;
 
-namespace System_test
-{
+using MotionIOLibrary;
+
+
+namespace System_test {
     public partial class Form1 : Form {
 
         //***********************************************************************
         // Constant definitions
         //***********************************************************************
-        const int SUCCESS       = 0;
-        const int COMBAUD       = 115200;
-        const int READ_TIMEOUT  = 10000;   // timeout for read reply (10 seconds)
+        const int SUCCESS      = 0;
 
         //***********************************************************************
         // Constant definitions
         //*********************************************************************** 
         UInt32 global_error;
 
-        public Form1()
-        {
+        FPGA_uP_IO FPGA_uP_IO = new FPGA_uP_IO();
+
+        public Form1() {
             InitializeComponent();
         }
 
         //***********************************************************************
         // User functions
         //*********************************************************************** 
+        // do_command : execute motion system command
+        // ==========
+        //
+        //public FPGA_uP_IO.ErrorCode do_command(string command) {
+
+        //    DebugWindow.AppendText(command + Environment.NewLine);
+        //    serialPort1.WriteLine(command);
+        //    DebugWindow.AppendText("Wating for reply" + Environment.NewLine);
+        //    int status = get_reply();
+
+        //    return 0;
+        //}
+
+        //*********************************************************************** 
         // get_reply : Read a status/data reply from LLcontrol subsystem
         //
-        public Int32 get_reply()
-        {
-            string reply;
-            Int32 status;
+        //public Int32 get_reply() {
+        //    string reply;
+        //    int status;
 
-            //serialPort1.DiscardInBuffer();
-            serialPort1.ReadTimeout = READ_TIMEOUT;
-            try
-            {
-                reply = serialPort1.ReadLine();
-            }
-            catch (TimeoutException)
-            {
-                DebugWindow.AppendText("ReadLine timeout fail" + Environment.NewLine);
-                return -1;
-            }
-            DebugWindow.AppendText("Reply = " + reply);
-            return SUCCESS;
-        }
- 
+        //    //serialPort1.DiscardInBuffer();
+        //    serialPort1.ReadTimeout = READ_TIMEOUT;
+        //    try {
+        //        reply = serialPort1.ReadLine();
+        //    }
+        //    catch (TimeoutException) {
+        //        DebugWindow.AppendText("ReadLine timeout fail" + Environment.NewLine);
+        //        return -1;
+        //    }
+        //    DebugWindow.AppendText("Reply = " + reply);
+        //    return SUCCESS;
+        //}
+
         //***********************************************************************
         // Window interface functions
         //*********************************************************************** 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e) {
+
             comboBox1.Items.Clear();
-            foreach (string s in SerialPort.GetPortNames())
-            {
-                comboBox1.Items.Add(s);
+            string[] ports = SerialPort.GetPortNames();
+            if (ports.Length > 0) {
+                foreach (string s in ports) {
+                    comboBox1.Items.Add(s);
+                }
+            } else {
+                DebugWindow.AppendText("No serial ports" + Environment.NewLine);
+                return;
             }
             comboBox1.SelectedIndex = 0;
-            serialPort1.BaudRate = COMBAUD;
+
             global_error = SUCCESS;
             Thread.Sleep(2000);
         }
 
-        private void tabPage1_Click(object sender, EventArgs e)
-        {
+        private void tabPage1_Click(object sender, EventArgs e) {
 
         }
 
-        private void exitToolStripMenuItem_Click_2(object sender, EventArgs e)
-        {
+        private void exitToolStripMenuItem_Click_2(object sender, EventArgs e) {
+
             serialPort1.Close();
             this.Close();
         }
 
-        private void aboutToolStripMenuItem_Click_1(object sender, EventArgs e)
-        {
+        private void aboutToolStripMenuItem_Click_1(object sender, EventArgs e) {
+
             MessageBox.Show("C# program to test Wattbot_nt control board", "WattBot-nt");
         }
 
         private void button1_Click(object sender, EventArgs e) {
-            string com_port = comboBox1.SelectedItem.ToString();
-            try {
-                serialPort1.PortName = com_port;
-                serialPort1.Open();
+
+            if (comboBox1.SelectedItem == null) {
+                DebugWindow.AppendText("No COM port selected " + Environment.NewLine);
+                return;
             }
-            catch {
+            string com_port = comboBox1.SelectedItem.ToString();
+            int baud_rate = Convert.ToInt32(comboBox2.SelectedItem);
+
+            FPGA_uP_IO.ErrorCode status = FPGA_uP_IO.Init_comms(com_port, baud_rate);
+
+            if (status != FPGA_uP_IO.ErrorCode.NO_ERROR) {
                 DebugWindow.AppendText("Cannot open " + com_port + Environment.NewLine);
                 return;
             }
-            DebugWindow.AppendText(com_port + "now open" + Environment.NewLine);
+            DebugWindow.AppendText(com_port + " now open" + Environment.NewLine);
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            String command = "p 5 ";
+        private void button2_Click_1(object sender, EventArgs e) {
+
+            String command = "Pu 5";
 
             DebugWindow.AppendText(command + Environment.NewLine);
-            serialPort1.WriteLine(command);
+            FPGA_uP_IO.do_command(command);
             DebugWindow.AppendText("Wating for reply" + Environment.NewLine);
-            int status = get_reply();
+           // int status = FPGA_uP_IO.get_reply();
         }
     }
 }
